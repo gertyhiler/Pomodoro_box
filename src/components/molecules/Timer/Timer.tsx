340927200132eeba23c5107701b0498f2b288efd
Andrew Korobka <tryuserrus@gmail.com>
2023-04-20 23:31:09 +0300

    fix: Timer not updated when settings changed

--- src/components/molecules/Timer/Timer.tsx
+++ src/components/molecules/Timer/Timer.tsx
@@ -1,12 +1,20 @@
-import React, { type FC } from 'react'
+import React, { useEffect, type FC } from 'react'
 import { convertTimeToString } from '../../../utils/convertTimeToString'
 import { ClockSection } from '../../atoms/ClockSection'
 import { useTimerTaskStore } from '../../organisms/TimerTask/store/useTimerTaskStore'
 // import { type ITimer } from './ITimer'
 import style from './style.module.css'
+import { useSettingStore } from '../../../store/setting/useSettingStore'
 
 export const Timer: FC = () => {
-  const { min, sec, isBreak, isPause, isRun } = useTimerTaskStore(state => state)
+  const { min, sec, isBreak, isPause, isRun, setMin, setSec } = useTimerTaskStore(state => state)
+  const { timeOneTomato } = useSettingStore(state => state)
+  useEffect(() => {
+    if (isRun) return
+    setMin(timeOneTomato)
+    setSec(0)
+  }, [timeOneTomato])
+
   return (
     <span className={`${style.clock} ${isRun && !isPause && isBreak ? style.isBreak : isRun && !isPause && !isBreak ? style.isWork : ''}`}>
       <ClockSection figure={convertTimeToString(min)}/> : <ClockSection figure={convertTimeToString(sec)}/>
